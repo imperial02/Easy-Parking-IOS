4541abc40bce30d9fe80a3be1293dcabe8c0bc5e
Luibomir Drevych <LDrevych@gmail.com>
2018-09-23 21:44:01 +0300

    [+] Update LocationManager [+] Fix bug with google maps now GooglePlace and GoogleMaps work correct [ˆ] Refactor code

---
+++ Easy - Parking/Easy - Parking/Controllers/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  Easy - Parking
+//
+//  Created by Любчик on 9/16/18.
+//  Copyright © 2018 Easy-Parking. All rights reserved.
+//
+
+import UIKit
+import GooglePlaces
+import GoogleMaps
+
+final class MapViewController: UIViewController {
+
+    //MARK: - IBOutlet
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    //MARK: - Variables
+    private let camera = GMSCameraPosition.camera(withLatitude: 49.8383, longitude: 24.0232, zoom: 10.0)
+    private let locationManager = LocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        mapView.camera = camera
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        mapView.settings.compassButton = false
+    }
+    
+    @IBAction func searchLocation(_ sender: Any) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        self.present(autocompleteController, animated: true, completion: nil)
+    }
+    
+}
+
+extension MapViewController: LocationManagerDelegate {
+    
+    func didChange(status: CLAuthorizationStatus) {
+        <#code#>
+    }
+    
+    func didReceiveUserLocation(_ location: CLLocation) {
+        print("\(location)")
+    }
+    
+    func didReceiveError(_ error: Error) {
+        print("\(error.localizedDescription)")
+    }
+    
+    func showAlertForRestrictedCase() {
+        DispatchQueue.main.async { [weak self] in
+            guard let `self` = self else { return }
+            AlertHelper.showAlert(on: self, title: Constants.restrictedAlertTitle,
+                                  message: Constants.restrictedAlertMessage,
+                                  buttonTitle: Constants.restrictedAlertButtonName,
+                                  buttonAction: { guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else { return }
+                                    UIApplication.shared.open(settingsUrl)
+            }, showCancelButton: false)
+        }
+    }
+    
+    
+}
+
+extension MapViewController: GMSAutocompleteViewControllerDelegate {
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        let camera = GMSCameraPosition.camera(withLatitude: place.coordinate.latitude, longitude: place.coordinate.longitude, zoom: 15.0)
+        self.mapView.camera = camera
+        print("Place name: ", place.name)
+        print("Place address: ", place.formattedAddress as Any)
+        print("Place attributions: ", place.attributions as Any)
+        print("Place Latitude: ", place.coordinate.latitude)
+        print("Place Longitude: ", place.coordinate.longitude)
+        self.dismiss(animated: true, completion: nil) // dismiss after select place
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("ERROR AUTO COMPLETE \(error)")
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        self.dismiss(animated: true, completion: nil) 
+    }
+    
+    func didRequestAutocompletePredictions(forResultsController resultsController: GMSAutocompleteResultsViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(forResultsController resultsController: GMSAutocompleteResultsViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+}
+
+extension MapViewController: GMSMapViewDelegate {
+    
+}
